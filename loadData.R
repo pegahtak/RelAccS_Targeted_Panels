3b2e158792755252c06a16fefb4f6f6c59a4f944
pegah taklifi <pegah.taklifi@gmail.com>
2022-02-12 17:38:26 +0330

    load TCGA Raw data

--- loadData.R
+++ loadData.R
@@ -1,1 +1,7 @@
 ### load data and collapse samples
+library(readxl)
+
+### read TCGA pan caner data set 
+### obtained form https://gdc.cancer.gov/about-data/publications/ATACseq-AWG
+### download link: https://api.gdc.cancer.gov/data/d6e5358d-491c-4776-8043-b5e49b96706e
+data<- readRDS("TCGA_panCancer.rds") 

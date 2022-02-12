f657c19968f50753c2558795a0b2dfb6c3613887
pegah taklifi <pegah.taklifi@gmail.com>
2022-02-12 18:01:29 +0330

    normalize to library size

--- loadData.R
+++ loadData.R
@@ -5,3 +5,11 @@ library(readxl)
 ### obtained form https://gdc.cancer.gov/about-data/publications/ATACseq-AWG
 ### download link: https://api.gdc.cancer.gov/data/d6e5358d-491c-4776-8043-b5e49b96706e
 data<- readRDS("TCGA_panCancer.rds") 
+data<- data[ , -1:-7]
+
+sequencing_stat<- read_excel("TCGA-ATAC_DataS1_DonorsAndStats_v4.xlsx" , sheet = 3)
+colnames(sequencing_stat)<- sequencing_stat[37, ]
+sequencing_stat<- sequencing_stat[ -1:-37, ]
+
+### normalize to total library size
+data<- (data/ as.numeric(sequencing_stat$Final_DeDup_reads))

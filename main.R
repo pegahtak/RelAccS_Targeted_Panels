3a1b6c864d0e2c4c828330519be855ad55513aee
pegah taklifi <pegah.taklifi@gmail.com>
2022-02-13 14:26:57 +0330

    added cancer IDs before for loop

--- main.R
+++ main.R
@@ -18,6 +18,7 @@ source("BRCA_subTypes.R") ## gets subTypes of breast cancer
 cancerGroups<- as.data.frame(table(cancerTypes))
 write.table(cancerGroups ,"cancerTypesInfo.txt" , sep="\t" , row.names = FALSE
             , quote = FALSE)
+
 IDs<- rep(0 , nrow(set))
 for ( j in 1:length(cancerGroups))
 {

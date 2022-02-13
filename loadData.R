e6b10ac37be2dde70655da0e2a83d109b33c1bd9
pegah taklifi <pegah.taklifi@gmail.com>
2022-02-13 11:46:38 +0330

    moved TCGA counts rds file to data folder

--- loadData.R
+++ loadData.R
@@ -4,7 +4,7 @@ library(purrr)
 ### read TCGA pan caner data set 
 ### obtained form https://gdc.cancer.gov/about-data/publications/ATACseq-AWG
 ### download link: https://api.gdc.cancer.gov/data/d6e5358d-491c-4776-8043-b5e49b96706e
-data<- readRDS("TCGA_panCancer.rds") 
+data<- readRDS("data/TCGA_panCancer.rds") 
 data<- data[ , -1:-7]
 
 sequencing_stat<- read_excel("TCGA-ATAC_DataS1_DonorsAndStats_v4.xlsx" , sheet = 3)
